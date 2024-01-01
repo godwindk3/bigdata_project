1d1012eb3db7b3bf49c2e1813ee8165715853667
anbish <minhanthongminh@gmail.com>
2024-01-01 16:57:38 +0700

    update comments for backend, process, load

--- pipeline/load/mongoload.py
+++ pipeline/load/mongoload.py
@@ -16,60 +16,68 @@ logging.basicConfig(level=logging.INFO,
 
 logger = logging.getLogger(__name__)
 
-app=typer.Typer()
+app = typer.Typer()
 
+logger = logging.getLogger(__name__)
 
-# Connect to MongoDB and pizza_data database
-logger=logging.getLogger(__name__)
 
 class MongoLoader:
+    # Init connection URL to Kafka server and mongodb's server
     def __init__(self) -> None:
-        self.server=settings.KAFKA_SERVER
-        self.connection_url=settings.MONGO_DB_URL
-   
+        self.server = settings.KAFKA_SERVER 
+        self.connection_url = settings.MONGO_DB_URL
+
+    # Connect to mongodb
     def init_db(self):
         client = MongoClient(self.connection_url)
-        self.client=client
-        
+        self.client = client
+
         try:
             client.admin.command('ping')
             logger.info('Ping db successfully.')
         except Exception as e:
-            logger.exception(e,stack_info=True)
+            logger.exception(e, stack_info=True)
             exit(1)
-        db=self.client.main
-        self.db=db
+        db = self.client.main # Get or create database with name "main"
+        self.db = db
 
+    # start consumer topic
     def __start_consumer(self):
-        consume_topic=settings.ANALYZED_TOPIC
-        self.consumer=KafkaConsumer(consume_topic,bootstrap_servers=self.server)
+        consume_topic = settings.ANALYZED_TOPIC # Get consumer topic's name
+        self.consumer = KafkaConsumer(
+            consume_topic, bootstrap_servers=self.server)
         logger.info(f'Started Kafka Consumer on topic :{consume_topic}')
 
+
+    # get message in the consumer
     def recieve_upstream(self):
         for message in self.consumer:
-            raw_message=message.value
-            yield raw_message
+            raw_message = message.value # Get the message in byte format
+            yield raw_message # yield the message receive from the consumer
 
-    def push_to_db(self,json_message,collection_name):
-        if collection_name not in self.db.list_collection_names():
+    # Push the chat messages to db
+    def push_to_db(self, json_message, collection_name):
+        if collection_name not in self.db.list_collection_names(): # Check if collection_name is exist in mongodb's server, if not => create collection_name
             self.db.create_collection(collection_name)
-        
-        collection=self.db.get_collection(collection_name)
-        response=collection.insert_one(json_message)
+
+        collection = self.db.get_collection(collection_name) # get collection
+        response = collection.insert_one(json_message) # add the message in json form to collection
         logger.info(f'Inserted message with ID of:{response.inserted_id}')
-        return response.inserted_id
-    
+        return response.inserted_id # return the id of the inserted message
+
+
+    # create the process pipe
     def __start_session(self):
-        self.init_db()
-        self.__start_consumer()
-        for message in self.recieve_upstream():
-            json_message=orjson.loads(message)
-            message_type=json_message['info_type']
-            if message_type=='VIDEO_STATIC_INFO':
-                self.push_to_db(json_message,'info')
-            if message_type=='VIDEO_LIVE_MESSAGE':
-                self.push_to_db(json_message,'live')
-        
+        self.init_db() # Init and connect to the mongodb, kafka server
+        self.__start_consumer() # Start the consumer topic
+        for message in self.recieve_upstream(): # Get the message from the consumer topic
+            json_message = orjson.loads(message) # Parse the message from bytes into JSON
+            message_type = json_message['info_type'] # get the type of message
+            if message_type == 'VIDEO_STATIC_INFO': # First type: video informations => push into "info" collection
+                self.push_to_db(json_message, 'info')
+            if message_type == 'VIDEO_LIVE_MESSAGE': # Second type: live chat message => push into "live" collection
+                self.push_to_db(json_message, 'live')
+
     def load(self):
         try:
             self.__start_session()
@@ -78,16 +86,11 @@ class MongoLoader:
             logger.error(e)
 
 
-
 @app.command()
 def run():
-    loader=MongoLoader()
+    loader = MongoLoader()
     loader.load()
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     app()
-
-
-    
-
